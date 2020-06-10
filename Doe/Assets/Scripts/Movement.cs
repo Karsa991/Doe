90b82acf0e0c687fdd2dc81d779e620f9a439670
Karsa991 <64644605+Karsa991@users.noreply.github.com>
2020-06-11 00:30:04 +0300

    MovementScript-Camera

---
+++ Doe/Assets/Scripts/Movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed=2f;
+
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //GetAxisRaw gt thelw i times na einai akrivos(-1,-0,1)
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+    //fixed
+    private void FixedUpdate()
+    {
+
+        rb.MovePosition(rb.position + movement * moveSpeed *Time.fixedDeltaTime);      
+    }
+}
